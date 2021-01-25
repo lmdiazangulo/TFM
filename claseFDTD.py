87fc89d0bc50350e560dc9049091d558e34fc308
Alberto Prados <77152539+Alber6@users.noreply.github.com>
2021-01-25 13:28:29 +0100

    Update claseFDTD.py
    A ver que ocurre

--- claseFDTD.py
+++ claseFDTD.py
@@ -2,7 +2,7 @@ import numpy as np
 from math import pi, sin, exp
 from matplotlib import pyplot as plt
 
-#Bastante extraño xdddd dsadasdasdasdasd
+#Editando a mitad
 class FDTD:
     def __init__(self, ncells, nsteps, ddx, t_0, s_0, epsilon_r, sigma, start_m, end_m):
         self.ncells=ncells
